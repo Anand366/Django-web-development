019647a35986b736ee17dfb328d5d09a6f84f241
Anand366 <anandkumarverma369@gmail.com>
2026-01-25 17:38:56 +0530

    Introduction part adding html and about, contact and home page.

---
+++ first_project/first_project/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def about(request):
+    return HttpResponse("Hello i am about page")
+
+def contact(request):
+    return HttpResponse(
+        "Hello i am <b>Contact page</b><br>"
+        "Mobile No. 564496541985<br>"
+        "Email :- anandrajashwin356@gmail.com<br>"
+        "Address :- chandi chowk, Pune, India"
+    )
+
+def Home(request):
+    return render(request, 'index.html')
